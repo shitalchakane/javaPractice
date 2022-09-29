57d18f33053f9ce2c4df56d0119782c10e924f5b
002HGU744 <shitalchakane@gmail.com>
2022-09-29 22:28:34 +0530

    Data Type check

---
+++ DataType.java
@@ -0,0 +1,20 @@
+public class DataType {
+    public static void main(String []args){
+        int intType = 26;
+        char charType = 'S';
+        boolean isAvailable = false;
+        short shortType = 4;
+        byte byteType = 2;
+        float floatType = 2.14f;
+        double doubleType = 4.987464;
+
+        System.out.println("Intger "+intType);
+        System.out.println("Character "+charType);
+        System.out.println("Boolean "+isAvailable);
+        System.out.println("Short "+shortType);
+        System.out.println("Byte "+ byteType);
+        System.out.println("Float "+ floatType);
+        System.out.println("Double "+doubleType);
+
+    }
+}
